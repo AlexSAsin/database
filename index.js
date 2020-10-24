cfba862d3bfb5011e2e721fa997728e435ef1c90
unknown <vip.casin@gmail.com>
2020-10-24 12:22:01 +0300

    1

--- index.js
+++ index.js
@@ -1,84 +1,58 @@
 require('dotenv').config()
-const { Client } = require('pg')
-const client = new Client({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+const express = require('express')
+const pool = require('./config/db')
+const app = express()
+
+app.route('/now').get(async (req, res) => {
+    const pgclient = await pool.connect()
+    const { rows }=await pgclient.query('SELECT now() as now')
+    await pgclient.release()
+    res.send(rows[0].now)
 })
 
-const name = '1 or 1 = 1'
-
-client.connect()
-async function addCar() {
-    const car = {
-        brandID: 1,
-        model: '420i',
-        cost: 25000000,
-        year: 2019,
-        isAvaible: true,
-    }
+app.route('/user_order/:id').get(async (req,res) => {
+    
+    let pgclient
     try {
-        // start transction
-        await client.query('BEGIN')
-        const res = await client.query(
-            `
-        INSERT INTO car (brand_id,model,cost,year_of_creation,is_avaible) VALUES
-        ($1,$2,$3,$4,$5) RETURNING id
-    `,
-            [car.brandID, car.model, car.cost, car.year, car.isAvaible]
-        )
-
-        const carID = res.rows[0].id
-        const resManager = await client.query(`
-    SELECT *
-    FROM manager
-    WHERE car_id IS NULL
-    LIMIT 1
-    `)
-        const managerID = resManager.rows[0].id
-
-        await client.query(
-            `
-        UPDATE manager 
-        SET car_id = $1
-        WHERE id = $2
-    `,
-            [carID, managerID]
-        )
-        await client.query('COMMIT')
-    } catch (err) {
-        client.query('ROLLBACK')
-        throw err
+    //id from URL
+    pgclient = await pool.connect()
+    const { id }=req.params
+    const { rows } =await pgclient.query(`SELECT id, client_id, created_at
+    FROM public.order_
+    WHERE client_id = $1
+    ORDER BY created_at DESC`,[id])
+    res.send(rows)
+    }catch (err){
+    res.status(500).send({
+    error: err.message
+})
     } finally {
-        client.end()
+        await pgclient.release()
+        console.log('close db connection')
     }
-}
-addCar()
-// client
-//     .query('SELECT NOW()')
-//     .then((result) => console.log(result))
-//     .catch((e) => console.error(e.stack))
-//     .then(() => client.end())
+})
 
-// 1 по указанному id менять статус на false
-// 2 по указанному id снижать цену на 10 %
-
-// 1 получить все id машин старше 2018
-// 2 снизить цену на полученные авто на 5%
-
-// 1 добавить новый автомобиль
-// 2 получить одного свободного менеджера
-// 3 назначить ему для продажи новый автомобиль
-
-// client.connect()
-// client.query(`
-//     SELECT *
-//     FROM student
-//     WHERE name = $1
-//     `,[name],function(err,res){
-//     console.log(err,res)
-//     client.end()
-// })
-console.log(1)
+app.route('/make_order/:id').post(async (req, res) => {
+    let pgclient
+    try{
+    pgclient = await pool.connect()
+    const { id }=req.params
+    const { rows } =await pgclient.query(`
+    INSERT INTO order_ (client_id) VALUES ($1) RETURNING id
+    `, [id])
+    const orderID =rows[0].id
+    res.send({
+        order_id: orderID
+    })
+} catch (err){
+    res.status(500).send({
+        error: err.message
+    })
+        } finally {
+            await pgclient.release()
+            console.log('close db connection')
+        }
+    })
+    app.listen(8080, (r)=>{
+    console.log('Server started (http://localhost:8080)')
+})
--- table.sql
+++ table.sql
@@ -1,29 +1,51 @@
-CREATE TABLE brand (
+CREATE TABLE menu (
 	id SERIAL PRIMARY KEY,
-	name varchar (255) NOT NULL,
-	country varchar (255) NOT NULL
-);
-CREATE TABLE car (
-	id SERIAL PRIMARY KEY,
-	brand_ID INTEGER NOT NULL REFERENCES brand(id),
-	model varchar (255) NOT NULL,
-	cost money,
-	year_of_creation NUMERIC(4,0) NOT NULL,
-	is_avaible boolean NOT NULL,
+	name varchar(255) NOT NULL,
+	price money NOT NULL,
+	description varchar(3000),
+	weight INTEGER NOT NULL,
 	created_at TIMESTAMP NOT NULL DEFAULT now(),
-	updated_at TIMESTAMP NOT NULL DEFAULT now(),
-	deleted_at TIMESTAMP
+	updated_at TIMESTAMP NOT NULL DEFAULT now()
 );
-INSERT INTO brand (name, country) VALUES
-('BMW','Germany'),
-('Lada','Russia'),
-('Honda','Japan'),
-('Tesla','USA');
 
-INSERT INTO car(brand_id,model, cost,year_of_creation,
-			   is_avaible) VALUES
-(1,'520D',2000000,2017,true),
-(2,'XRay',800000,2018,true),
-(4,'Model S', 12500000,2020,false),
-(1,'M5',7000000,2018,true);
+INSERT INTO menu (name, price, weight) VALUES
+('Пицца пепперони', 15, 700),
+('Салат', 10, 3000),
+('Шаурма', 2, 200);
+
+CREATE TABLE client (
+	id SERIAL PRIMARY KEY,
+	name varchar(255) NOT NULL,
+	address varchar(1000) NOT NULL,
+	phone varchar(11) NOT NULL
+);
+
+INSERT INTO client (name, address, phone) VALUES
+('Иван', 'Боголюбова, 21, кв 14', '88005553535'),
+('Николай', 'Университетская, 2, кв 11', '83004441122'),
+('Вера', 'Пушкина, 4, кв 27', '89153002222');
+
+CREATE TABLE order_ (
+	id SERIAL PRIMARY KEY,
+	client_id INTEGER NOT NULL REFERENCES client(id),
+	created_at TIMESTAMP NOT NULL DEFAULT now()
+);
+
+INSERT INTO order_ (client_id) VALUES 
+(1),(1),(1),(2);
+
+CREATE TABLE order_menu (
+	order_id INTEGER NOT NULL REFERENCES order_(id),
+	menu_id INTEGER NOT NULL REFERENCES menu(id),
+	count INTEGER NOT NULL,
+	price money NOT NULL,
+	PRIMARY KEY(order_id, menu_id)
+);
+
+INSERT INTO order_menu (order_id, menu_id, count, price) VALUES
+(1, 1, 1, 15),
+(1, 2, 1, 10),
+(2, 1, 2, 30),
+(3, 3, 6, 12),
+(4, 1, 2, 30);
  
